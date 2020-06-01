e8b194512951b11f5be7d839a4496cad74e6d71f
andresgeo96 <andresgeo96@gmail.com>
2020-06-01 11:39:07 -0500

    vista inicio-home

--- cocoApp/src/app/home/home.page.ts
+++ cocoApp/src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  slides = [
+    {
+      img: 'assets/kapak/kapakSlide1.png',
+      titulo: 'Bienvenido a Kapak'
+    },
+    {
+      img: 'assets/kapak/kapakSlide2.png',
+      titulo: 'La mejor tienda online de Ecuador'
+    },
+    {
+      img: "assets/kapak/kapakSlide3.jpg",
+      titulo: 'Kapak shop'
+    }
+  ];
 
   constructor() {}
+  ngOnInit() {
+  }
 
 }
